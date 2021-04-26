231b769838aee26c57592d5b8ed25c91b6d22594
Kadri Green <kadrigr@gmail.com>
2021-04-26 16:38:52 +0300

    Commit

--- src/main/java/ee/bcs/valiit/service1/BankService.java
+++ src/main/java/ee/bcs/valiit/service1/BankService.java
@@ -23,7 +23,7 @@ public class BankService {
     public String getBalance(String accountNr) {
         Account account= hibernateAccountRepository.getOne(accountNr);
      // return "Your balance is: " + bankRepository.getBalance(accountNr); SQLi näide
-       return "Your balance is: " + account.getBalance();
+       return "Kontojääk on " + account.getBalance()+ " EUR.";
 
     }
 
@@ -32,39 +32,39 @@ public class BankService {
             double balance = bankRepository.getBalance(accountNr);
             balance += amount;
             bankRepository.updateBalance(accountNr, balance);
-            return amount + " added to account " + accountNr + " new balance is: " + balance;
+            return amount + " EUR lisatud kontole " + accountNr + ". Uus konto jääk on " + balance+ " EUR.";
         } else {
-            return "Invalid amount";
+            return "Sisesta ainult positiivne summa.";
         }
     }
 
     public String withdrawMoney(String accountNr, Double amount) {
         if (amount > 0) {
             if (bankRepository.getBalance(accountNr) < 0) {
-                return "Not enough money";
+                return "Kontol pole piisavalt raha.";
             } else {
                 double balance = bankRepository.getBalance(accountNr) - amount;
                 bankRepository.updateBalance(accountNr, balance);
-                return amount + " withdrawn from account " + accountNr + " new balance is: " + balance;
+                return amount + " EUR välja võetud kontolt " + accountNr + ". Uus kontojääk on " + balance+" EUR.";
             }
         } else {
-            return "Invalid amount";
+            return "Sisesta ainult positiivne summa.";
         }
     }
 
     public String transferMoney(String fromAccount, String toAccount, Double amount) {
         if (amount > 0) {
             if (bankRepository.getBalance(fromAccount) - amount < 0) {
-                return "Not enough money";
+                return "Kontol pole piisavalt.";
             }
             double balance1 = bankRepository.getBalance(fromAccount) - amount;
             double balance2 = bankRepository.getBalance(toAccount) + amount;
             bankRepository.updateBalance(fromAccount, balance1);
             bankRepository.updateBalance(toAccount, balance2);
-            return amount + " transferred from account " + fromAccount + " to account " + toAccount +
-                    ". New balance1 is: " + balance1 + " . New balance2 is: " + balance2;
+            return amount + " EUR kantud kontolt " + fromAccount + " kontole " + toAccount +
+                    ". "+ fromAccount+" uus kontojääk on " + balance1 + "EUR. "+toAccount+" uus kontojääk on " + balance2+ " EUR.";
         } else {
-            return "Invalid amount";
+            return "Kontol pole piisavalt raha.";
         }
     }
 
